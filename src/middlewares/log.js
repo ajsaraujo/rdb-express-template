cc9a77e9138b88bc10f731b27cc6d554df138c3b
Allan <allanjuansil@gmail.com>
2020-09-02 10:41:39 -0300

    Adicionar corpos da requisição e resposta

--- src/middlewares/log.js
+++ src/middlewares/log.js
@@ -2,6 +2,20 @@ import morgan from 'morgan';
 import path from 'path';
 import fs from 'fs';
 
+const REQUEST_DATA = '":req[header] :req-body (:req[content-length] bytes)"\n';
+const RESPONSE_DATA = '":res[header] :res-body (:res[content-length] bytes)"\n';
+
+const DEVELOPMENT_LOG_FORMAT = '":method :url :status :response-time[0] ms"\n'
+    + REQUEST_DATA
+    + RESPONSE_DATA
+    + '\n\n';
+
+const PRODUCTION_LOG_FORMAT = '":remote-addr - :remote-user [:date[web]]"\n'
+    + '":method :url HTTP/:http-version :status :response-time[0] ms"\n'
+    + REQUEST_DATA
+    + RESPONSE_DATA
+    + '":referrer" ":user-agent"\n\n';
+
 function whenEverythingIsFine(req, res) {
     return res.statusCode < 400;
 }
@@ -14,18 +28,25 @@ function createStream() {
     return stream;
 }
 
+function createMorganTokens() {
+    morgan.token('req-body', (req, res) => JSON.stringify(req.body));
+    morgan.token('res-body', (req, res) => JSON.stringify(res.body));
+}
+
 function log() {
-    let mode;
+    let format;
     const options = { skip: whenEverythingIsFine };
 
+    createMorganTokens();
+
     if (process.env.NODE_ENV === 'production') {
-        mode = 'common';
+        format = PRODUCTION_LOG_FORMAT;
         options.stream = createStream();
     } else {
-        mode = 'dev';
+        format = DEVELOPMENT_LOG_FORMAT;
     }
 
-    return morgan(mode, options);
+    return morgan(format, options);
 }
 
 export default log;
