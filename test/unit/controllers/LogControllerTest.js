428ed7bca8fc8cabaa789ea411800124445d4c26
Allan <allanjuansil@gmail.com>
2020-09-07 13:15:40 -0300

    Testar método get

--- test/unit/controllers/LogControllerTest.js
+++ test/unit/controllers/LogControllerTest.js
@@ -1,4 +1,5 @@
 import LogController from '../../../src/controllers/LogController';
+import { expect } from 'chai';
 
 describe('LogController', () => {
     let Log;
@@ -7,7 +8,7 @@ describe('LogController', () => {
 
     beforeEach(() => {
         sandbox = createSandbox();
-        Log = { create: sandbox.stub() };
+        Log = { create: sandbox.stub(), find: sandbox.stub() };
         logController = new LogController(Log);
     });
 
@@ -16,6 +17,38 @@ describe('LogController', () => {
             await logController.create('O servidor papocou');
 
             expect(Log.create.calledWith({ content: 'O servidor papocou' })).to.be.true;
+        });
+    });
+
+    describe('get()', () => {
+        let req;
+        let res;
+
+        beforeEach(() => {
+            const mocks = TestUtils.mockReqRes(sandbox);
+            req = mocks.req;
+            res = mocks.res;
+        });
+
+        it('should return 200 and find all logs', async () => {
+            const logs = [{ content: 'O servidor papocou' }, { content: 'Eh resenha soh kkk' }];
+
+            Log.find.resolves(logs);
+
+            await logController.get(req, res);
+
+            expect(Log.find.getCall(0).args.length).to.equal(0);
+            expect(res.status.calledWith(200)).to.be.true;
+            expect(res.json.calledWith(logs)).to.be.true;
+        });
+
+        it('should return 500 if an error is thrown', async () => {
+            Log.find.rejects({ message: 'Rapaz tá tudo pegando fogo' });
+
+            await logController.get(req, res);
+
+            expect(res.status.calledWith(500)).to.be.true;
+            expect(res.json.calledWith({ message: 'Rapaz tá tudo pegando fogo' })).to.be.true;
         });
     });
 
