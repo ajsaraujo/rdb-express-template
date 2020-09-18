0ee5d10f19ba887601b592c541fcfd1ed062bc33
Allan <allanjuansil@gmail.com>
2020-09-18 14:40:56 -0300

    Adicionar rota para obtenção de logs

--- src/controllers/LogController.js
+++ src/controllers/LogController.js
@@ -10,6 +10,15 @@ class LogController {
             console.log(`Erro ao criar log: ${message}`);
         }
     }
+
+    async getAll(req, res) {
+        try {
+            const logs = await this.Log.findAll();
+            return res.status(200).json(logs);
+        } catch ({ message }) {
+            return res.status(500).json({ message: `Erro ao buscar logs: ${message}.` });
+        }
+    }
 }
 
 export default LogController;
---
+++ src/routes/logRoute.js
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+import Log from '../models/Log';
+import LogController from '../controllers/LogController';
+
+const router = Router();
+const logController = new LogController(Log);
+
+router.get('/', (req, res) => logController.getAll(req, res));
+
+export default { router, name: '/log' };
